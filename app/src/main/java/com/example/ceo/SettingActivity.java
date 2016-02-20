beb78af56f1ec5e9df8dc5b1143abbfc4c838a43
xhz636 <240032960@qq.com>
2016-02-20 18:14:33 +0800

    添加不同颜色识别功能 完善参数界面

--- app/src/main/java/com/example/ceo/SettingActivity.java
+++ app/src/main/java/com/example/ceo/SettingActivity.java
@@ -2,6 +2,7 @@ package com.example.ceo;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -16,26 +17,32 @@ public class SettingActivity extends Activity {
     private EditText ET_Black_G;
     private EditText ET_Black_B;
     private Button Btn_Black;
+    private View View_Black;
     private EditText ET_White_R;
     private EditText ET_White_G;
     private EditText ET_White_B;
     private Button Btn_White;
+    private View View_White;
     private EditText ET_Red_R;
     private EditText ET_Red_G;
     private EditText ET_Red_B;
     private Button Btn_Red;
+    private View View_Red;
     private EditText ET_Blue_R;
     private EditText ET_Blue_G;
     private EditText ET_Blue_B;
     private Button Btn_Blue;
+    private View View_Blue;
     private EditText ET_Yellow_R;
     private EditText ET_Yellow_G;
     private EditText ET_Yellow_B;
     private Button Btn_Yellow;
+    private View View_Yellow;
     private EditText ET_Green_R;
     private EditText ET_Green_G;
     private EditText ET_Green_B;
     private Button Btn_Green;
+    private View View_Green;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,26 +58,32 @@ public class SettingActivity extends Activity {
         ET_Black_G = (EditText) findViewById(R.id.black_g);
         ET_Black_B = (EditText) findViewById(R.id.black_b);
         Btn_Black = (Button) findViewById(R.id.black_check);
+        View_Black = findViewById(R.id.view_black);
         ET_White_R = (EditText) findViewById(R.id.white_r);
         ET_White_G = (EditText) findViewById(R.id.white_g);
         ET_White_B = (EditText) findViewById(R.id.white_b);
         Btn_White = (Button) findViewById(R.id.white_check);
+        View_White = findViewById(R.id.view_white);
         ET_Red_R = (EditText) findViewById(R.id.red_r);
         ET_Red_G = (EditText) findViewById(R.id.red_g);
         ET_Red_B = (EditText) findViewById(R.id.red_b);
         Btn_Red = (Button) findViewById(R.id.red_check);
+        View_Red = findViewById(R.id.view_red);
         ET_Blue_R = (EditText) findViewById(R.id.blue_r);
         ET_Blue_G = (EditText) findViewById(R.id.blue_g);
         ET_Blue_B = (EditText) findViewById(R.id.blue_b);
         Btn_Blue = (Button) findViewById(R.id.blue_check);
+        View_Blue = findViewById(R.id.view_blue);
         ET_Yellow_R = (EditText) findViewById(R.id.yellow_r);
         ET_Yellow_G = (EditText) findViewById(R.id.yellow_g);
         ET_Yellow_B = (EditText) findViewById(R.id.yellow_b);
         Btn_Yellow = (Button) findViewById(R.id.yellow_check);
+        View_Yellow = findViewById(R.id.view_yellow);
         ET_Green_R = (EditText) findViewById(R.id.green_r);
         ET_Green_G = (EditText) findViewById(R.id.green_g);
         ET_Green_B = (EditText) findViewById(R.id.green_b);
         Btn_Green = (Button) findViewById(R.id.green_check);
+        View_Green = findViewById(R.id.view_green);
     }
 
     void LoadSetting() {
@@ -82,36 +95,42 @@ public class SettingActivity extends Activity {
         ET_Black_R.setText(Color_R.toString());
         ET_Black_G.setText(Color_G.toString());
         ET_Black_B.setText(Color_B.toString());
+        View_Black.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
         Color_R = pref.getInt("White_R", 255);
         Color_G = pref.getInt("White_G", 255);
         Color_B = pref.getInt("White_B", 255);
         ET_White_R.setText(Color_R.toString());
         ET_White_G.setText(Color_G.toString());
         ET_White_B.setText(Color_B.toString());
+        View_White.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
         Color_R = pref.getInt("Red_R", 255);
         Color_G = pref.getInt("Red_G", 0);
         Color_B = pref.getInt("Red_B", 0);
         ET_Red_R.setText(Color_R.toString());
         ET_Red_G.setText(Color_G.toString());
         ET_Red_B.setText(Color_B.toString());
+        View_Red.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
         Color_R = pref.getInt("Blue_R", 0);
         Color_G = pref.getInt("Blue_G", 0);
         Color_B = pref.getInt("Blue_B", 255);
         ET_Blue_R.setText(Color_R.toString());
         ET_Blue_G.setText(Color_G.toString());
         ET_Blue_B.setText(Color_B.toString());
+        View_Blue.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
         Color_R = pref.getInt("Yellow_R", 255);
         Color_G = pref.getInt("Yellow_G", 255);
         Color_B = pref.getInt("Yellow_B", 0);
         ET_Yellow_R.setText(Color_R.toString());
         ET_Yellow_G.setText(Color_G.toString());
         ET_Yellow_B.setText(Color_B.toString());
+        View_Yellow.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
         Color_R = pref.getInt("Green_R", 0);
         Color_G = pref.getInt("Green_G", 255);
         Color_B = pref.getInt("Green_B", 0);
         ET_Green_R.setText(Color_R.toString());
         ET_Green_G.setText(Color_G.toString());
         ET_Green_B.setText(Color_B.toString());
+        View_Green.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
     }
 
     void CreateButton() {
@@ -127,6 +146,7 @@ public class SettingActivity extends Activity {
                 editor.putInt("Black_R", Color_R);
                 editor.putInt("Black_G", Color_G);
                 editor.putInt("Black_B", Color_B);
+                View_Black.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
                 editor.commit();
             }
         });
@@ -142,6 +162,7 @@ public class SettingActivity extends Activity {
                 editor.putInt("White_R", Color_R);
                 editor.putInt("White_G", Color_G);
                 editor.putInt("White_B", Color_B);
+                View_White.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
                 editor.commit();
             }
         });
@@ -157,6 +178,7 @@ public class SettingActivity extends Activity {
                 editor.putInt("Red_R", Color_R);
                 editor.putInt("Red_G", Color_G);
                 editor.putInt("Red_B", Color_B);
+                View_Red.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
                 editor.commit();
             }
         });
@@ -172,6 +194,7 @@ public class SettingActivity extends Activity {
                 editor.putInt("Blue_R", Color_R);
                 editor.putInt("Blue_G", Color_G);
                 editor.putInt("Blue_B", Color_B);
+                View_Blue.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
                 editor.commit();
             }
         });
@@ -187,6 +210,7 @@ public class SettingActivity extends Activity {
                 editor.putInt("Yellow_R", Color_R);
                 editor.putInt("Yellow_G", Color_G);
                 editor.putInt("Yellow_B", Color_B);
+                View_Yellow.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
                 editor.commit();
             }
         });
@@ -202,6 +226,7 @@ public class SettingActivity extends Activity {
                 editor.putInt("Green_R", Color_R);
                 editor.putInt("Green_G", Color_G);
                 editor.putInt("Green_B", Color_B);
+                View_Green.setBackgroundColor(Color.rgb(Color_R, Color_G, Color_B));
                 editor.commit();
             }
         });
--- app/src/main/java/com/example/ceo/TestModeActivity.java
+++ app/src/main/java/com/example/ceo/TestModeActivity.java
@@ -1,8 +1,12 @@
 package com.example.ceo;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -30,6 +34,14 @@ public class TestModeActivity extends Activity implements CvCameraViewListener2{
     private ColorBlobDetector mDetector;
     private CameraBridgeViewBase mOpenCvCameraView;
 
+    private SharedPreferences pref;
+    private Button Find_Black;
+    private Button Find_White;
+    private Button Find_Red;
+    private Button Find_Blue;
+    private Button Find_Yellow;
+    private Button Find_Green;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +49,77 @@ public class TestModeActivity extends Activity implements CvCameraViewListener2{
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+        CreateButton();
+    }
+
+    void CreateButton() {
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        Find_Black = (Button) findViewById(R.id.find_black);
+        Find_Black.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer Color_R, Color_G, Color_B;
+                Color_R = pref.getInt("Black_R", 0);
+                Color_G = pref.getInt("Black_G", 0);
+                Color_B = pref.getInt("Black_B", 0);
+                ColorTest(Color_R, Color_G, Color_B);
+            }
+        });
+        Find_White = (Button) findViewById(R.id.find_white);
+        Find_White.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer Color_R, Color_G, Color_B;
+                Color_R = pref.getInt("White_R", 0);
+                Color_G = pref.getInt("White_G", 0);
+                Color_B = pref.getInt("White_B", 0);
+                ColorTest(Color_R, Color_G, Color_B);
+            }
+        });
+        Find_Red = (Button) findViewById(R.id.find_red);
+        Find_Red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer Color_R, Color_G, Color_B;
+                Color_R = pref.getInt("Red_R", 0);
+                Color_G = pref.getInt("Red_G", 0);
+                Color_B = pref.getInt("Red_B", 0);
+                ColorTest(Color_R, Color_G, Color_B);
+            }
+        });
+        Find_Blue = (Button) findViewById(R.id.find_blue);
+        Find_Blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer Color_R, Color_G, Color_B;
+                Color_R = pref.getInt("Blue_R", 0);
+                Color_G = pref.getInt("Blue_G", 0);
+                Color_B = pref.getInt("Blue_B", 0);
+                ColorTest(Color_R, Color_G, Color_B);
+            }
+        });
+        Find_Yellow = (Button) findViewById(R.id.find_yellow);
+        Find_Yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer Color_R, Color_G, Color_B;
+                Color_R = pref.getInt("Yellow_R", 0);
+                Color_G = pref.getInt("Yellow_G", 0);
+                Color_B = pref.getInt("Yellow_B", 0);
+                ColorTest(Color_R, Color_G, Color_B);
+            }
+        });
+        Find_Green = (Button) findViewById(R.id.find_green);
+        Find_Green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer Color_R, Color_G, Color_B;
+                Color_R = pref.getInt("Green_R", 0);
+                Color_G = pref.getInt("Green_G", 0);
+                Color_B = pref.getInt("Green_B", 0);
+                ColorTest(Color_R, Color_G, Color_B);
+            }
+        });
     }
 
     @Override
@@ -80,11 +163,10 @@ public class TestModeActivity extends Activity implements CvCameraViewListener2{
         }
     };
 
-    private void ColorTest() {
-        mBlobColorRgba.val[0] = 0;
-        mBlobColorRgba.val[1] = 0;
-        mBlobColorRgba.val[2] = 0;
-        mBlobColorRgba.val[3] = 0;
+    private void ColorTest(int R, int G, int B) {
+        mBlobColorRgba.val[0] = R;
+        mBlobColorRgba.val[1] = G;
+        mBlobColorRgba.val[2] = B;
         mBlobColorHsv = converScalarRgba2Hsv(mBlobColorRgba);
         mDetector.setHsvColor(mBlobColorHsv);
         Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE);
@@ -98,7 +180,6 @@ public class TestModeActivity extends Activity implements CvCameraViewListener2{
         SPECTRUM_SIZE = new Size(200, 64);
         CONTOUR_COLOR = new Scalar(255,0,0,255);
         mDetector = new ColorBlobDetector();
-        ColorTest();
     }
 
     public void onCameraViewStopped() {
@@ -111,13 +192,6 @@ public class TestModeActivity extends Activity implements CvCameraViewListener2{
         List<MatOfPoint> contours = mDetector.getContours();
         Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
         return mRgba;
-    }
-
-    private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
-        Mat pointMatRgba = new Mat();
-        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
-        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
-        return new Scalar(pointMatRgba.get(0, 0));
     }
 
     private Scalar converScalarRgba2Hsv(Scalar rgbaColor) {
